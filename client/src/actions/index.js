f31a74bdc6bb9ed5165cdfa0b4388cf7536801e2
vinod10 <vinodkumar@kwiqpick.com>
2017-12-28 21:39:44 +0530

    added redux form

---
+++ client/src/actions/index.js
@@ -0,0 +1,11 @@
+export function signinUser({ email, password}) {
+    //submit email/password to  the server
+
+    //if request is good...
+    // -Update state to indicate user is authenticated
+    // -save the jwt token
+    // -redirect the route '/feature'
+
+    //if request is bad...
+    // -show the error to user
+}
---
+++ client/src/components/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+    render(){
+        return(
+            <nav className="navbar navbar-light">
+                <ul className="nav navbar-nav">
+                    <li className="nav-item">
+                        Signin
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Header;
